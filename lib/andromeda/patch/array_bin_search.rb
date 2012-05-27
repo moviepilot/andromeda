0e7810115342392b2659ed09932481e56a3ebf95
Stefan Plantikow <stefan.plantikow@googlemail.com>
2012-05-27 11:17:58 +0200

    Converted BinSearch into mixin

--- lib/andromeda/patch/array_bin_search.rb
+++ lib/andromeda/patch/array_bin_search.rb
@@ -1,83 +1,81 @@
-class ::Array
+module BinSearch
 
-  class Tag
-    attr_reader :key
-    attr_accessor :value
+  LIN_BITS = 6
 
-    def initialize(key, value = nil)
-      @key   = key
-      @value = value
+  module Methods
+    # Binary search for the first elem that is leq elem in this array in the range
+    # (low..high-1)
+    #
+    # By default, <=> is used to compare elements.  Alternatively, a comparator
+    # may be specified as block parameter
+    #
+    # The array is expected to be sorted in descending order.
+    #
+    # @param [Object] elem elem to search for
+    # @param [Fixnum] low lower bound (inclusive)
+    # @param [Fixnum] high upper bound (inclusive, -1 for last element)
+    # @return [Fixnum] index of first occurence leq than elem in self, or -1 if not found
+    #
+    def bin_search_desc(elem, low = 0, high = -1)
+      high = size - 1 if high < 0
+      if block_given?
+        then _bin_search_desc(elem, low, high) { |a, b| yield a, b }
+        else _bin_search_desc(elem, low, high) { |a, b| a <=> b } end
     end
 
-    def untagged ; value end
-
-    def <=>(other)
-      return key <=> other.key
+    # Binary search for the first elem that is leq elem in this array in the range
+    # (low..high-1)
+    #
+    # Elements are compared using <=> after mapping them using the provided block
+    #
+    # The array is expected to be sorted in descending order.
+    #
+    # @param [Object] elem elem to search for
+    # @param [Fixnum] low lower bound (inclusive)
+    # @param [Fixnum] high upper bound (inclusive, -1 for last element)
+    # @return [Fixnum] index of first occurence leq than elem in self, or -1 if not found
+    #
+    def bin_search_desc_by(elem, low = 0, high = -1)
+      high = size - 1 if high < 0
+      _bin_search_desc(elem, low, high) { |a, b| (yield a) <=> (yield b) }
     end
 
-    def to_s
-      "Tag(#{key.to_s} => #{value.to_s})"
-    end
+    private
 
-    alias_method :inspect, :to_s
-  end
-
-  # Binary search for the first elem that is leq elem in this array in the range
-  # (low..high-1)
-  #
-  # The array is expected to be sorted in descending order.
-  #
-  # @param [Object] elem elem to search for
-  # @param [Fixnum] low lower bound (inclusive)
-  # @param [Fixnum] high upper bound (inclusive, -1 for last element)
-  # @return [Fixnum] index of first occurence leq than elem in self, or -1 if not found
-  #
-  def bin_search(elem, low = 0, high = -1)
-    high = size - 1 if high < 0
-    _bin_search elem, low, high
-  end
-
-  # Wraps all objects in self into ::Array::Tag instances using
-  # the provided block to extract a key
-  #
-  # @return [self]
-  #
-  def tag! ; map! { |e| Tag.new (yield e), e } end
-
-  # Untags Array::Tag instances, i.e. replaces them with their value
-  #
-  # @return [self]
-  #
-  def untag! ; map! { |e| e.untagged } end
-
-  private
-
-  def _bin_search(elem, low, high)
-    last = -1
-    while low <= high
-      sz = high - low
+    def _bin_search_desc(elem, low, high)
+      last = -1
       # On 2012 cpus, linear search is slightly faster than binary search
       # if the number of searched elements is in the range of 50-100 elts
-      return _lin_search elem, low, high if (sz >> 6)
-      mid_index = low + (sz >> 1)
-      if (elem <=> self[mid_index]) == -1
-        low  = mid_index + 1
+      sz = high - low
+      if (sz >> LIN_BITS)
+        cur_index = low
+        while cur_index <= high
+          cmp = yield elem, self[cur_index]
+          if cmp == -1
+            then cur_index += 1
+            else return cur_index end
+        end
+        return -1
       else
-        last = mid_index
-        high = mid_index - 1
+        while low <= high
+          mid_index = low + (sz >> 1)
+          cmp = yield elem, self[mid_index]
+          if cmp == -1
+            low  = mid_index + 1
+          else
+            last = mid_index
+            high = mid_index - 1
+          end
+          sz -= 1
+        end
+        return last
       end
     end
-    return last
-  end
 
-  def _lin_search(elem, low, high)
-    cur_index = low
-    while cur_index <= high
-      if (elem <=> self[cur_index]) == -1
-        then cur_index += 1
-        else return cur_index end
-    end
-    return -1
-  end
+  end # module Methods
 
+end # module BinSeach
+
+class ::Array
+  include ::BinSearch::Methods
 end
